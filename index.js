e353ebecc0269e02f29239a625211e0bfa332998
Sean-Crowley <seancrowley033@gmail.com>
2021-02-23 16:07:14 +0000

    added .env file

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Inert = require("@hapi/inert");
 const Vision = require("@hapi/vision");
 const Handlebars = require("handlebars");
 const Cookie = require("@hapi/cookie");
+const env = require("dotenv");
+
+env.config();
 
 const server = Hapi.server({
   port: 3000,
@@ -33,8 +36,8 @@ async function init() {
   });
   server.auth.strategy("session", "cookie", {
     cookie: {
-      name: "donation",
-      password: "password-should-be-32-characters",
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
       isSecure: false,
     },
     redirectTo: "/",
