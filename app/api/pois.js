8c377758c5d13c7323e99ce7027e2773a546d797
Sean-Crowley <seancrowley033@gmail.com>
2021-05-15 15:06:27 +0100

    Added findOne POI endpoint with Boom

--- app/api/pois.js
+++ app/api/pois.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Poi = require('../models/poi');
+const Boom = require("@hapi/boom");
 
 const  Pois = {
     find: {
@@ -8,6 +9,21 @@ const  Pois = {
         handler: async function (request, h) {
             const pois = await Poi.find();
             return pois;
+        },
+    },
+
+    findOne: {
+        auth: false,
+        handler: async function (request, h) {
+            try {
+                const poi = await Poi.findOne({ _id: request.params.id });
+                if (!poi) {
+                    return Boom.notFound("No POI with this ID");
+                }
+                return poi;
+            } catch (err) {
+                return Boom.notFound("No POI with this ID");
+            }
         },
     },
 };
--- routes-api.js
+++ routes-api.js
@@ -1,5 +1,6 @@
 const Pois = require('./app/api/pois');
 
 module.exports = [
-    { method: 'GET', path: '/api/pois', config: Pois.find }
+    { method: 'GET', path: '/api/pois', config: Pois.find },
+    { method: 'GET', path: '/api/pois/{id}', config: Pois.findOne },
 ]
