b45f162cf2e6aca8925115083a0262a5a106a586
Sean-Crowley <seancrowley033@gmail.com>
2021-05-15 16:10:26 +0100

    Added a test for create

--- test/poi-api-test.js
+++ test/poi-api-test.js
@@ -7,13 +7,13 @@ suite("POI API tests", function () {
     test("get pois", async function () {
         const response = await axios.get("http://localhost:3000/api/pois");
         const pois = response.data;
-        assert.equal(2, pois.length);
+        assert.equal(4, pois.length);
     })
 
     test("get one POI", async function () {
         let response = await axios.get("http://localhost:3000/api/pois")
         const pois = response.data;
-        assert.equal(2, pois.length);
+        assert.equal(4, pois.length);
 
         const onePoiUrl = "http://localhost:3000/api/pois/" + pois[0]._id;
         response = await  axios.get(onePoiUrl);
@@ -21,5 +21,24 @@ suite("POI API tests", function () {
 
         assert.equal(onePoi.name, "GreenWay");
         assert.equal(onePoi.description, "Walk by the River Suir");
+    })
+
+    test("create a Poi", async function () {
+        const poisUrl = "http://localhost:3000/api/pois";
+        const newPoi = {
+            name: "Cliff Walk",
+            description: "demo2",
+            lat: 45.8,
+            lon: 56.7,
+            category: "Cycle Path",
+        };
+
+        const response = await  axios.post(poisUrl, newPoi);
+        const returnedPoi = response.data;
+        assert.equal(201, response.status);
+
+        assert.equal(returnedPoi.name, "Cliff Walk");
+        assert.equal(returnedPoi.description, "demo2")
+        assert.equal(returnedPoi.category, "Cycle Path")
     })
 })
